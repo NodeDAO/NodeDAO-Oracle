c5ac9d85e1eec957c4333d1549826ab4652f2ac0
RenShiWei <ren_shi_wei@qq.com>
2023-02-06 14:54:39 +0800

    oracle contracts modify

--- src/contracts/oracle.ts
+++ src/contracts/oracle.ts
@@ -8,7 +8,7 @@ import {config} from "../config/config";
 import beaconOracleAbi from './abi/beaconOracle.json';
 import vNFTAbi from './abi/vNFT.json';
 
-const BEACON_ORACLE_CONTRACT_ADDR = "0x7dC41e563766Bbb062A0b14Aece4d137d8F3F4fc";
+const BEACON_ORACLE_CONTRACT_ADDR = "0xD3DB4B79f75d12DdC81F32A2F466E60Af885cE1b";
 const VNFT_CONTRACT_ADDR = "0xA55506acca8f2A655436CB140962269AB87d3370";
 
 const executionLayerAddr = config.executionLayerAddr;
@@ -50,6 +50,10 @@ export function reportBeacon(
 
 export function getMerkleRoot(): Promise<string> {
     return oracleContract.merkleTreeRoot();
+}
+
+export function verifyNftValue(proof: string[], pubkey: string, balance: ethers.BigNumber, tokenId: ethers.BigNumber): Promise<boolean> {
+    return oracleContract.verifyNftValue(proof, pubkey, balance, tokenId);
 }
 
 //------------------------------ vNFT ----------------------------------
